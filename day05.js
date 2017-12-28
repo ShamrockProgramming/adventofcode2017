c9e5e7801a4a8bca42106cfdb034d54292222bc1
mrfleming <ericfleming8@gmail.com>
2017-12-27 23:16:19 -0600

    finsihed part 2 of day5

--- day05.js
+++ day05.js
@@ -7,9 +7,16 @@ for(let n=0;n<numbers.length-1;n++){
     let i = numbers[n].indexOf("\r");
     numbers[n] = Number(numbers[n].substring(0,i));
 }
-//console.log(numbers);
-let len = numbers.length-1
-console.log("The length of the array is "+len);
+
+let len = numbers.length-1;
+//console.log("The length of the array is "+len);
+
+let secondtry = [];
+for(let c=0;c<=len;c++){
+    secondtry[c] = numbers[c];
+}
+console.log(numbers.length === secondtry.length);
+
 let position = 0;
 let count =0;
 while(position < len && position > -1){
@@ -25,3 +32,27 @@ while(position < len && position > -1){
 
 console.log("The position is "+position);
 console.log("The count is "+count);
+
+position = 0;
+count = 0;
+console.log("====== NEW GAME ======");
+
+while(position < len && position > -1){
+    let oldpos = position;
+    let jump = secondtry[position];
+    position += jump;
+    if(jump >= 3){
+        secondtry[oldpos]--;
+    }
+    else{
+        secondtry[oldpos]++;
+    }
+    count++;
+    if(count>100000000){
+        console.log("BROKEN");
+        break;
+    }
+}
+
+console.log("The new position is "+position);
+console.log("The new count is "+count);
